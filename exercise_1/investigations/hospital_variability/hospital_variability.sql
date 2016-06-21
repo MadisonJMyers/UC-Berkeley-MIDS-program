79986c62be916c30dca97fa05207c394cde85c5c
Madison J Myers <mjmyers@us.ibm.com>
2016-06-20 22:05:11 -0700

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -1,1 +1,6 @@
+#code in progress
+#not functioning
+#out of time
+
+SELECT DISTINCT a.measure_id, ROUND(a.sd_score, 2) as sd_score, a.range_score FROM (SELECT e.measure_id, STDDEV_POP(e.score) as sd_score, MAX(e.score) - MIN(e.score) as range_score FROM effective_care_tr e GROUP BY e.measure_id) a ORDER BY desc limit 10;
 
